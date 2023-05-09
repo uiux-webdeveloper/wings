d474b90a206508247d228d4d1e304589e3406f68
uiuxwebdeveloper <uiuxwebdeveloper5@gmail.com>
2023-05-09 13:35:15 +0530

    testimonials and other commit

--- script.js
+++ script.js
@@ -1,3 +1,41 @@
+$(document).ready(() => {
+	$(".testimonial-container").slick({
+		dots: false,
+		infinite: false,
+		speed: 300,
+		slidesToShow: 4,
+		slidesToScroll: 4,
+		responsive: [
+			{
+				breakpoint: 1024,
+				settings: {
+					slidesToShow: 3,
+					slidesToScroll: 3,
+					infinite: true,
+					dots: true,
+				},
+			},
+			{
+				breakpoint: 600,
+				settings: {
+					slidesToShow: 2,
+					slidesToScroll: 2,
+				},
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					slidesToShow: 1,
+					slidesToScroll: 1,
+				},
+			},
+			// You can unslick at a given breakpoint now by adding:
+			// settings: "unslick"
+			// instead of a settings object
+		],
+	});
+});
+
 const bars = document.querySelector(".header-wrapper .bars");
 const ul = document.querySelector(".header-wrapper ul");
 const line = document.querySelector(".bars .fa-bars");
